37f6ee358f15c22ec9892ea4fea967c40ababc51
Charles Eromose <okuekhamhene.gmail.com>
2024-12-04 14:59:56 +0100

    profilemodels

--- userapps/models.py
+++ userapps/models.py
@@ -1,7 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+GENDER = (
+    ('M', 'Male'),
+    ('F', 'Female'),
+)
 class Profile(models.Model):
-    def __str__(self):
-        pass
+        user = models.OneToOneField(User, on_delete=models.CASCADE)
+        phone = models.CharField(max_length=50)
+        fullname = models.CharField(max_length=255)
+        gender = models.CharField(max_length=10, choices=GENDER)
+        profile_pix = models.ImageField(upload_to='profile', blank=True, null=True)#pip install pillow to allow for image to be uploaded
+        
+        def __str__(self):
+            return self.fullname
  
